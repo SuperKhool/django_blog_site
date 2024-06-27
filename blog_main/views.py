e2c785d6e3ab07ab5823315589844cc031c1351a
superkhool <superkhool69@gmail.com>
2024-06-27 23:23:56 +0600

    templates set_up

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render, redirect
+from blogs.models import category ,Blog
 
-def home(request):
-    return render(request,'home.html')
+def home(request):   
+    category_list=category.objects.all()
+    featured_post=Blog.objects.filter(is_featured=True,status="Published").order_by('updated_at')
+    simple_post=Blog.objects.filter(is_featured=False,status="Published" ).order_by('updated_at')
+    context={
+        
+        'category_list':category_list,
+        'featured_post':featured_post,
+        'simple_post':simple_post
+            
+    }
+    return render(request,'home.html',context)
