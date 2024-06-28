cba3789ab66e054cc59fec5dd5a19c2e6a3bd0c7
superkhool <superkhool69@gmail.com>
2024-06-28 14:16:18 +0600

    404

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(z#rz(ztby$rp+j&y4c-cc8jw5^)2=dxxfl9^oq#onck6+m3wd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse ,redirect
+from django.shortcuts import render,HttpResponse ,redirect ,get_object_or_404
 from .models import *
 from . import models
 
@@ -6,10 +6,11 @@ def post_by_category(req,category_id):
     #fetch the data from category_id
     post=Blog.objects.filter(status="Published",category_id=category_id)
 
-    try:
-        categorys=category.objects.get(pk=category_id)
-    except:
-        return redirect('home')
+    # try:
+    #     categorys=category.objects.get(pk=category_id)
+    # except:
+    #     return redirect('home')
+    categorys=get_object_or_404(category,id=category_id)
         
     context={
         'post':post,
